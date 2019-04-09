99c51f670d8f171e2c7f475ff99fa534857a8b9d
yuguanglin <763086613@qq.com>
2019-04-09 14:58:57 +0800

    deploy-web07

--- deploy.sh
+++ deploy.sh
@@ -33,7 +33,7 @@ rm -f $TOMCAT_APP_PATH/webapps/order.war
 rm -f $TOMCAT_APP_PATH/webapps/order
 
 # 复制新的工程
-cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
+mv $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
 
 #cd $TOMCAT_APP_PATH/webapps/
 #mv order.war ROOT.war
