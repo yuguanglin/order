79fb346424fcbdeb148f3686a7143fbaa357036e
yuguanglin <763086613@qq.com>
2019-04-13 12:41:34 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -30,7 +30,7 @@ killTomcat
 #rm -rf $TOMCAT_APP_PATH/webapps/ROOT
 #rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
 rm -f $TOMCAT_APP_PATH/webapps/order.war
-rm -f $TOMCAT_APP_PATH/webapps/order
+rm -rf $TOMCAT_APP_PATH/webapps/order
 
 # 复制新的工程
 mv $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
